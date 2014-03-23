ee72fc4b35e3d2cee7d8c2345d21d771611f58cc
Robert Lord <robert@lord.io>
2014-03-23 16:33:00 -0400

    Move coquette extensions to separate file

--- js/coquette.js
+++ js/coquette.js
@@ -69,27 +69,6 @@
       }
     },
 
-    check_collision_offset: function(entity, x, y) {
-      var collisions = [];
-
-      // get all entity pairs to test for collision
-      var ents = this.c.entities.all();
-      // TODO HACKISH SOLUTION
-      // this just moves the center over, checks for collisions, and then moves
-      // it back
-      entity.center.x += x;
-      entity.center.y += y;
-      for (var i = 0, len = ents.length; i < len; i++) {
-        if (ents[i] !== entity && this.isColliding(entity, ents[i])) {
-          collisions.push(ents[i]);
-        }
-      }
-      entity.center.x -= x;
-      entity.center.y -= y;
-
-      return collisions;
-    },
-
     collision: function(entity1, entity2) {
       var collisionType;
       if (!isUncollisionOn(this.c.entities.all())) {
