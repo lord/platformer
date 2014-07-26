99802854ed3147e71f009b4747aeb6eccece73af
Robert Lord <robert@lord.io>
2014-07-25 23:14:01 -0700

    Add basic pixi js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,37 @@
 ;(function(exports) {
+  var stage, renderer, cat;
+  function setupPixi() {
+    stage = new PIXI.Stage(0x66FF99);
+    renderer = PIXI.autoDetectRenderer(400, 300);
+    document.body.appendChild(renderer.view);
+
+    var texture = PIXI.Texture.fromImage("/cat.png");
+    cat = new PIXI.Sprite(texture);
+
+    cat.anchor.x = 0.5;
+    cat.anchor.y = 0.5;
+
+    cat.position.x = 200;
+    cat.position.y = 150;
+
+    stage.addChild(cat);
+  }
+
+  function animate() {
+    requestAnimFrame(animate);
+    renderer.render(stage);
+    cat.rotation += 0.1;
+  }
+
   window.addEventListener('load', function() {
-    var meter = new FPSMeter({
-      heat: 1,
-      graph: 1,
-      history: 40,
-      interval: 500
-    });
-    exports.meter = meter;
+    // var meter = new FPSMeter({
+    //   heat: 1,
+    //   graph: 1,
+    //   history: 40,
+    //   interval: 500
+    // });
+    // exports.meter = meter;
+    setupPixi();
+    requestAnimFrame(animate);
   });
 })(this);
