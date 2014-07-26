58d30ec9f6bcc04d77846fbc1889e81785249ff7
Robert Lord <robert@lord.io>
2014-07-25 23:14:19 -0700

    Bye bye, FPS meter

--- js/app.js
+++ js/app.js
@@ -24,13 +24,6 @@
   }
 
   window.addEventListener('load', function() {
-    // var meter = new FPSMeter({
-    //   heat: 1,
-    //   graph: 1,
-    //   history: 40,
-    //   interval: 500
-    // });
-    // exports.meter = meter;
     setupPixi();
     requestAnimFrame(animate);
   });
