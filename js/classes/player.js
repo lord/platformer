5b4136418fb1059ee7c48ec97d5c4bf9d13c839d
Robert Lord <robert@lord.io>
2014-11-03 22:31:46 -0600

    Add velocity to player

--- js/classes/player.js
+++ js/classes/player.js
@@ -6,7 +6,13 @@
     this.addChild(sprite);
     this.w = 16;
     this.h = 16;
+    this.vx = 0;
+    this.vy = 0;
   };
   Player.prototype = new PIXI.DisplayObjectContainer();
+  Player.prototype.step = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+  };
   exports.Player = Player;
 })(this);
